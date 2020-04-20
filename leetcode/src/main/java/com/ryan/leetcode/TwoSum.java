1f103242cec14ed22509e4a0d15a2aa6c6791287
yuanls <lishuai.yuan@myfittime.com>
2020-04-20 16:50:18 +0800

    Two sum

--- leetcode/src/main/java/com/ryan/leetcode/TwoSum.java
+++ leetcode/src/main/java/com/ryan/leetcode/TwoSum.java
@@ -24,7 +24,7 @@ import java.util.Map;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
-public class twoSum {
+public class TwoSum {
 
     public static void main(String[] args) {
         int[] ints = twoSum(new int[]{2, 7, 11, 15}, 17);
