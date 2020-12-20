10f7abca940e5c6668dc7b574d37f469334e441f
yuanls <yuanls@126.com>
2020-12-20 11:09:05 +0800

    del

---
+++ src/main/java/com/ryan/designpattern/BeansTest.java
@@ -0,0 +1,70 @@
+package com.ryan.designpattern;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+public class BeansTest {
+
+    private boolean isDeleted;
+
+    private Boolean isDelete;
+
+    private int isInt;
+
+    private Integer isInteger;
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public Boolean getDelete() {
+        return isDelete;
+    }
+
+    public void setDelete(Boolean delete) {
+        isDelete = delete;
+    }
+
+    public int getIsInt() {
+        return isInt;
+    }
+
+    public void setIsInt(int isInt) {
+        this.isInt = isInt;
+    }
+
+    public Integer getIsInteger() {
+        return isInteger;
+    }
+
+    public void setIsInteger(Integer isInteger) {
+        this.isInteger = isInteger;
+    }
+
+         private static Random rand = new Random();
+        private static long t = System.currentTimeMillis();
+        static int getMoreData() {
+            System.out.println("begin to start compute");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("end to start compute. passed " + (System.currentTimeMillis() - t)/1000 + " seconds");
+            return rand.nextInt(1000);
+        }
+        public static void main(String[] args) throws Exception {
+            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(BeansTest::getMoreData);
+            Future<Integer> f = future.whenComplete((v, e) -> {
+                System.out.println(v);
+                System.out.println(e);
+            });
+            System.out.println(f.get());
+        }
+
+}
