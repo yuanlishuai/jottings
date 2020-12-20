5b9fe9877221225c7935f0f80108ec90be41219d
yuanls <yuanls@126.com>
2020-12-20 11:22:18 +0800

    reformat code

--- src/main/java/com/ryan/designpattern/BeansTest.java
+++ src/main/java/com/ryan/designpattern/BeansTest.java
@@ -46,25 +46,27 @@ public class BeansTest {
         this.isInteger = isInteger;
     }
 
-         private static Random rand = new Random();
-        private static long t = System.currentTimeMillis();
-        static int getMoreData() {
-            System.out.println("begin to start compute");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("end to start compute. passed " + (System.currentTimeMillis() - t)/1000 + " seconds");
-            return rand.nextInt(1000);
+    private static Random rand = new Random();
+    private static long t = System.currentTimeMillis();
+
+    static int getMoreData() {
+        System.out.println("begin to start compute");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        public static void main(String[] args) throws Exception {
-            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(BeansTest::getMoreData);
-            Future<Integer> f = future.whenComplete((v, e) -> {
-                System.out.println(v);
-                System.out.println(e);
-            });
-            System.out.println(f.get());
-        }
+        System.out.println("end to start compute. passed " + (System.currentTimeMillis() - t) / 1000 + " seconds");
+        return rand.nextInt(1000);
+    }
+
+    public static void main(String[] args) throws Exception {
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(BeansTest::getMoreData);
+        Future<Integer> f = future.whenComplete((v, e) -> {
+            System.out.println(v);
+            System.out.println(e);
+        });
+        System.out.println(f.get());
+    }
 
 }
