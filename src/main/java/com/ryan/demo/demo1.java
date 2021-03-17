d28cfae094da8551ed3dbaa3e5aec8f030b3d1a3
yuanls <yuanls@126.com>
2021-03-17 21:30:06 +0800

    code

---
+++ src/main/java/com/ryan/demo/demo1.java
@@ -0,0 +1,15 @@
+package com.ryan.demo;
+
+public class demo1 {
+
+
+    private static final String str = "dtd";
+
+    private static final Integer ll = 3;
+
+    public static void main(String[] args) {
+
+        System.out.println(str);
+        System.out.println(ll);
+    }
+}
