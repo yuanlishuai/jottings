1d98dcd3ea79956d3528dc1a4e6d2275628fe37f
yuanls <yuanls@126.com>
2021-04-04 17:33:57 +0800

    demo2

--- src/main/java/com/ryan/demo/demo1.java
+++ src/main/java/com/ryan/demo/demo1.java
@@ -1,15 +1,19 @@
 package com.ryan.demo;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 public class demo1 {
 
 
-    private static final String str = "dtd";
+    private String str = new String("dtd");
+    private String str2 = "dtd";
 
-    private static final Integer ll = 3;
+    private static final int ll = 00000000000000000000000000001011;
 
     public static void main(String[] args) {
-
-        System.out.println(str);
-        System.out.println(ll);
+        System.out.println(String.valueOf(ll));
+        System.out.println(Integer.toBinaryString(ll));
+        System.out.println(Integer.toString(ll));
     }
 }
